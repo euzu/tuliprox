1d30567408f1df8efd1ac3c09a9263864a853c11
euzu <euzu@proton.me>
2026-03-18 10:35:18 +0100

    episode handling for redirect

--- backend/src/api/endpoints/xtream_api.rs
+++ backend/src/api/endpoints/xtream_api.rs
@@ -265,6 +265,9 @@ async fn xtream_player_api_stream(
         return axum::http::StatusCode::BAD_REQUEST.into_response();
     }
 
+    let stream_ext = stream_ext.filter(|s| !s.is_empty())
+        .or_else(|| pli.get_container_extension().map(|e| concat_string!(".", e.as_ref())));
+
     let input = try_option_bad_request!(
         app_state.app_config.get_input_by_name(&pli.input_name),
         true,
@@ -363,8 +366,6 @@ async fn xtream_player_api_stream(
 
     let context = stream_req.context;
 
-    let stream_ext = stream_ext.filter(|s| !s.is_empty())
-        .or_else(|| pli.get_container_extension().map(|e| concat_string!(".", e.as_ref())));
     let redirect_params = RedirectParams {
         item: &pli,
         provider_id: pli.get_provider_id(),
